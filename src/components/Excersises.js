f078816326cd29d1dc443a659fbe55cabec4a3d0
Hamad Ur Rehman <ceerhamad@gmail.com>
2024-08-29 11:59:33 +0500

    creating navigation bar

---
+++ src/components/Excersises.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Excersises = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Excersises
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+function Home() {
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExcercises />
+      <Excercises />
+    </Box>
+  )
+}
+
+export default Home
