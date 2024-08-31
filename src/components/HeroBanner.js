ee4b6b956c11d62cacdbab355c1350fab906006f
Hamad Ur Rehman <ceerhamad@gmail.com>
2024-08-31 11:18:59 +0500

    adding horizontal scroll bar

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -7,34 +7,34 @@ const HeroBanner = () => {
     <Box sx={{
         mt:{lg:'212px' ,xs:'70px'},
         ml:{sm:'50px'},
-    }}position="relative" p='20px' >
+    }   }position="relative" p='20px' >
       <Typography color='#FF2625'
-      fontWeight="600" fontSize='26px'>
+        fontWeight="600" fontSize='26px'>
         Fitness club
       </Typography>
       <Typography fontWeight={700}
-      sx={{fontSize:{lg:'44px',xs:'40px'}
-      }}
-      mb="23px" mt="30px">
+        sx={{fontSize:{lg:'44px',xs:'40px'}
+        }}
+        mb="23px" mt="30px">
         Sweat, Smile <br /> and Repeat
       </Typography>
       <Typography fontSize="22px"
-      lineHeight='35px'mb={4}>
+        lineHeight='35px'mb={4}>
         Checkout the most effective excercises
       </Typography>
-      <Button variant="contained" color='error'
-      href='#excercises' 
-      sx={{backgroundColor:'#ff2625', padding:'15px'}}>Explore Excersises</Button>
+        <Button variant="contained" color='error'
+        href='#excercises' 
+        sx={{backgroundColor:'#ff2625', padding:'15px'}}>Explore Excersises</Button>
       <Typography fontWeight={600}
-      color='#ff2625'
-      sx={{
+        color='#ff2625'
+        sx={{
         opacity:0.1,
         display:{lg:  'block',xs:'none'},
-      }}
-      fontSize="200px">
+        }}
+        fontSize="200px">
         Excercise
       </Typography>
-      <img src={HeroBannerImage} alt='banner'className='hero-banner-img'></img>
+        <img src={HeroBannerImage} alt='banner'className='hero-banner-img'></img>
     </Box>
   )
 }
--- src/components/SearchExcercises.js
+++ src/components/SearchExcercises.js
@@ -1,10 +1,94 @@
-import React from 'react'
+import React, { useSyncExternalStore } from 'react'
+import { useEffect,useState } from 'react'
+import {Box,Button,Stack,Typography,TextField} from '@mui/material'
+import { fetchData,exerciseOption } from '../utils/fetchData'
+import HorizontalScrollbar from './HorizontalScrollbar'
 
 const SearchExcercises = () => {
+    const [search, setSearch] = useState('')
+    const [exercises, setExercises] = useState([])
+    const [bodyParts, setBodyParts] = useState([])
+
+    useEffect(() => {
+      const fetchExerciseData = async () =>{
+        const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList',exerciseOption)
+
+        setBodyParts(['all', ...bodyPartsData])
+      }
+      fetchExerciseData();
+      
+    }, [])
+    
+
+    const handleSearch = async() =>{
+        if(search){
+            const  exerciseData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOption)
+            
+            const SearchExcercises = exerciseData.filter(
+                (exercise) => exercise.name.toLowerCase().includes(search)
+                || exercise.target.toLowerCase().includes(search)
+                || exercise.bodyPart.toLowerCase().includes(search)
+                || exercise.equiptment.toLowerCase().includes(search)
+            );
+
+            setSearch('');
+            setExercises(searchedExcercises)
+        }
+
+        
+    }
+    
   return (
-    <div>
-      SearchExcercises
-    </div>
+    <Stack alignItems='center' mt='37px'
+        justifyContent={'center'}
+        p='20px'>
+        SearchExcercises
+        <Typography fontWeight={700} sx={{fontSize:{lg:'44px',xs:'30px'}}}
+        marginBottom='50px' textAlign='center'>
+        Awesome Excercises You <br />
+        Should Know
+        </Typography>
+
+        <Box position='relative'mb='72px'>
+            <TextField 
+            sx={{
+                input:{
+                    fontWeight:'700',
+                    border:'none',
+                    borderRadius:'4px'
+                },
+                width:{lg:'800px', xs:'350px'},
+                backgroundColor:'#fff',
+                borderRadius:'40px',
+            }}
+            height='76px'
+            value="search"
+            onChange={(e) => setSearch(e.target.value.toLowerCase)}
+            placeholder='Search Excercises'
+            type='text'            />
+            <Button className='search-btn'
+            sx={{
+                backgroundColor:'#FF2625',
+                color:'#fff',
+                textTransform:'none',
+                width:{lg:'175px', xs:'80px'},
+                fontSize:{lg:'20px' , xs:'14px'},
+                height:'56px',
+                position:'absolute',
+                right:'0px',
+
+            }}
+            onClick={handleSearch}
+            >
+                Search
+            </Button>
+        </Box>
+        <Box sx={{position: 'relative', width:'100%', p:'20px'}}>
+            <HorizontalScrollbar  data = {bodyParts}/>
+
+        </Box>
+    </Stack>
+    
   )
 }
 
